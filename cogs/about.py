bc19d46f36d7a0d29a43a2984f14a659d2f50b85
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2022-10-16 16:53:04 +0700

    Add /donate

--- cogs/about.py
+++ cogs/about.py
@@ -72,6 +72,29 @@ class About(commands.Cog):
         """ /about """
         await self.async_slash_about(interaction)
 
+    @app_commands.command(
+        name='donate',
+        description="Show donation address to HellBot"
+    )
+    async def slash_donate(
+        self, interaction: discord.Interaction
+    ) -> None:
+        """ /donate """
+        await interaction.response.send_message(f"{interaction.user.mention} loading donation info...")
+        try:
+            embed = discord.Embed(
+                title='Donation',
+                description="Thank you for checking. Any amount of donation is appreciated!",
+                timestamp=datetime.now()
+            )
+            for k, v in self.bot.config['donate'].items():
+                embed.add_field(name=k.upper(), value=v, inline=False)
+            embed.set_footer(text=f'/donate | requested by {interaction.user.name}',
+                                icon_url=self.bot.user.display_avatar)
+            embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.display_avatar)
+            await interaction.edit_original_response(content=None, embed=embed)
+        except Exception:
+            traceback.print_exc(file=sys.stdout)
 
     async def cog_load(self) -> None:
         pass
